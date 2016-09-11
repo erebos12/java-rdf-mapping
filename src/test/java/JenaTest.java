5258713f88866e1694e0c8f91c8a75a8e9f093b1
Alex Sahm <alexander.sahm@gmx.de>
2016-09-11 16:20:23 +0200

    converting function from object to RDF

--- src/test/java/JenaTest.java
+++ src/test/java/JenaTest.java
@@ -1,43 +1,78 @@
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.vocabulary.VCARD;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.impl.PropertyImpl;
 import org.junit.Test;
 
 public class JenaTest {
+	
+	class Name extends PropertyImpl
+	{
 
-    // some definitions
-    static String tutorialURI  = "http://hostname/rdf/tutorial/";
-    static String briansName   = "Brian McBride";
-    static String briansEmail1 = "brian_mcbride@hp.com";
-    static String briansEmail2 = "brian_mcbride@hpl.hp.com";
-    static String title        = "An Introduction to RDF and the Jena API";
-    static String date         = "23/01/2001";
+		public Name(String uri) {
+			super(uri);
+			// TODO Auto-generated constructor stub
+		}
+	}
+	
 	
 	@Test
-	public void test() {
+	public void test() throws Exception {
 		  // some definitions
-        String personURI    = "http://somewhere/JohnSmith";
-        String givenName    = "John";
-        String familyName   = "Smith";
-        String fullName     = givenName + " " + familyName;
-        // create an empty model
+        String personURI    = "http://somewhere/MohamedLee";
+        String name = "Mohamed Lee";        
+        String age = "34";
+        String ssn = "456423456";
+        String maritalStatus = "single";
+        String email = "mohamed.lee@gmx.de";
+        String address = "Main Street 1";
+                
+        Person person = new Person();
+        person.setName(name);
+        person.setAge(age);
+        person.setAddress(address);
+        person.setEmail(email);
+        person.setMaritalStatus(maritalStatus);
+        person.setSsn(ssn);
+        
+        HashMap<String, String> fieldValuePairs = new HashMap<String, String>();
+        fieldValuePairs = getFieldValuePairs(person);      
+        
         Model model = ModelFactory.createDefaultModel();
-
-        // create the resource
-        //   and add the properties cascading style
-        org.apache.jena.rdf.model.Resource johnSmith 
+             
+        Resource p = model.createResource(personURI);
+        
+        for (Entry<String, String> entry : fieldValuePairs.entrySet())
+        {            
+            System.out.println(entry.getKey() + " = " + entry.getValue());
+            PropertyImpl prop = new PropertyImpl("file:" + entry.getKey());
+            p.addProperty(prop, entry.getValue().toString());
+        }
+        /*Resource MohamedLee 
           = model.createResource(personURI)
-                 .addProperty(VCARD.FN, fullName)
-                 .addProperty(VCARD.N, 
-                              model.createResource()
-                                   .addProperty(VCARD.Given, givenName)
-                                   .addProperty(VCARD.Family, familyName));
+                 .addProperty(VCARD.ADR, address)
+                 .addProperty(VCARD.EMAIL, email)
+                 .addProperty(VCARD.NAME, name);*/
         
         // now write the model in XML form to a file
         model.write(System.out);
 	}
 
-	
+	public HashMap<String, String> getFieldValuePairs(Object obj) throws Exception {
+	    Class<?> objClass = obj.getClass();
+        HashMap<String, String> fieldValuePairs = new HashMap<String, String>();
+	    Field[] fields = objClass.getFields();
+	    for(Field field : fields) {
+	        String fieldName = field.getName();
+	        Object fieldValue = field.get(obj);
+	        fieldValuePairs.put(fieldName, fieldValue.toString());	        	        
+	    }
+		return fieldValuePairs;
+	}
 	
 	
 }
