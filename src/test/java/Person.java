dfb3563d30edf94df137cf5601a98ef3688d3644
Alex Sahm <alexander.sahm@gmx.de>
2016-09-11 16:22:05 +0200

    added plain object Person

---
+++ src/test/java/Person.java
@@ -0,0 +1,61 @@
+
+public class Person{
+	
+	public String name;
+	public String ssn;
+	public String email;
+	public String address;
+	public String maritalStatus;
+	public String age;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getMaritalStatus() {
+		return maritalStatus;
+	}
+
+	public void setMaritalStatus(String maritalStatus) {
+		this.maritalStatus = maritalStatus;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public Person() {
+	}
+}
